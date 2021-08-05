f7db8761a387e3c38c6e41030636eb4d04a85977
mitko <epzhod@gmail.com>
2021-08-05 14:00:37 +0100

    implemented to index

--- components/footer.js
+++ components/footer.js
@@ -1,208 +1,235 @@
 const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML = `
 
-        <style>
+<style>
 
-        /* Fonts added Heebo- variable; ProSans - normal, bold, italic*/
-        @font-face {
-            font-family: Heebo;
-            src: url(./fonts/Heebo-VariableFont_wght.ttf);}
+/* Fonts added Heebo- variable; ProSans - normal, bold, italic*/
+@font-face {
+    font-family: Heebo;
+    src: url(./fonts/Heebo-VariableFont_wght.ttf);}
 
-        @font-face {
-            font-family: ProSans;
-            src: url(./fonts/SourceSansPro-Regular.ttf)
-        }
+@font-face {
+    font-family: ProSans;
+    src: url(./fonts/SourceSansPro-Regular.ttf)
+}
 
-        :root {
-            
-            /* Defining the 2021 colors in the root as variables*/
+:root {
+    
+    /* Defining the 2021 colors in the root as variables*/
 
-            --pear: #cde43a ;
-            --seagreen: #58bc82 ;
-            --prussian: #13293d;
-            --uranian: #c4e3fd;
-            --dodger: #4895ef;
-            --cobalt: #1446a0;
-        }
+    --pear: #cde43a ;
+    --seagreen: #58bc82 ;
+    --prussian: #13293d;
+    --uranian: #c4e3fd;
+    --dodger: #4895ef;
+    --cobalt: #1446a0;
+}
 
-        /* General element styles begin */
-        
-        body {
-            margin: 0px;
-            border: 0px;
-            padding: 0px;
-            background-color: var(--uranian);
-        }
-        
-        
-        h1, h2, h3, h4, h5, h6 {
-            font-family: Heebo;
-        }
-        
-        p {
-            font-family: ProSans;
-            font-size:larger;
-        }
-        /* General element styles end */
+/* General element styles begin */
 
-        /* Footer element styling */
+body {
+    margin: 0px;
+    border: 0px;
+    padding: 0px;
+}
 
 
-        .footer-container {
-            margin: 0.50rem;
-            border-color: white;
-            border-style: solid;
-            padding: 0.5rem;
-        }
+h1, h2, h3, h4, h5, h6 {
+    font-family: Heebo;
+}
 
-        .footer {
-            flex-grow: 1;
-            display: grid;
-            gap: 0.5rem;
-            grid-template-columns: repeat( auto-fit , minmax(240px, 1fr));
-            justify-content: center;
-        }
+p {
+    font-family: ProSans;
+    font-size:larger;
+}
 
-        .footer-heading {
-            background-color: #1446a0;
-            padding: 0.25rem;
-            gap: 2px;
-        }
+a:link {color:var(--pear);text-decoration:none;}
+a:visited {color:var(--pear);text-decoration:none;}
+a:hover {color: var(--dodger) ;text-decoration:underline;}
 
-        .footer-heading ul {
-            list-style-type: none;
-            list-style-position: inside;
-            padding: 0.1rem;
-            margin-inline-start: 0px;
-            text-align: right;
-            gap: 0.5rem;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-        }
 
-        .footer-heading p {
-            text-align: center;
-        }
+/* General element styles end */
 
-        .navlinks {
-        }
+/* Footer element styling */
 
-        .sponsors {
-        }
 
-        .social {
-            background-color: #1446a0;
-            display: grid;
-            grid-template-rows: 2fr 1fr;
-        }
+.footer-container {
+    border-color: white;
+    border-style: solid;
+    padding: 0.5rem;
+    background-color: var(--prussian);
+    position: absolute;
+    width: 98vw;
+    bottom: 0px;
+    left: 0px;
+}
 
-        .social p{
-            text-align: center;
-        }
+.footer {
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: repeat( auto-fit , minmax(240px, 1fr));
+    justify-content: space-evenly;
+}
 
-        .social-box{
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-        }
+.footer-heading {
+    grid-column: span 1;
+    background-color: var(--prussian);
+    padding: 0.25rem;
+    gap: 2px;
+}
 
-        .social-box p {
-            flex-grow: 1;
-        }
+.footer-heading ul {
+    list-style-type: none;
+    list-style-position: inside;
+    padding: 0.1rem;
+    margin-inline-start: 0px;
+    text-align: right;
+    gap: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+}
 
-        .social-box ul {
-            flex-grow: 4;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-evenly;
-            align-content: center;
-            list-style-type: none;
-            list-style-position: inside;
-            padding: 0.1rem;
-            margin-inline-start: 0px;
-            text-align: center;
-        }
+.footer-heading p {
+    text-align: center;
+    font-size: 20px;
+    font-family: Heebo;
+    color: var(--pear);
+}
 
-        .renervate-logo{
-            background-color: #1446a0;
-            background-image: url(./footer_elements/teamphoto.png);
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
+.social {
+    grid-column: span 2;
+    background-color: var(--prussian);
+    display: grid;
+    grid-template-rows: 1fr 0.5fr 1.5fr;
+}
 
-        .team-photo {
+.social p{
+    text-align: center;
+}
 
-        }
+.social-box{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+}
 
-        .team-photo img{
-            
-        }
+.social-box p {
+    flex-grow: 1;
+    font-size: 20px;
+    font-family: Heebo;
+    color: var(--pear);
+}
 
-        .icon {
+.social-box ul {
+    flex-grow: 4;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-content: center;
+    list-style-type: none;
+    list-style-position: inside;
+    padding: 0.1rem;
+    margin-inline-start: 0px;
+    text-align: center;
+}
 
-        }
+.social-photo{
+    background-color: var(--prussian);
+    background-repeat: no-repeat;
+    background-position: center;
+}
 
-        .icon:hover {
-            /* This method need the conversion from here: https://codepen.io/sosuke/pen/Pjoqqp */
-            filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
-        }
+.team-photo {
+    background-image: url(./footer_elements/teamlarge.jpeg);
+    background-size: cover;
+    margin: 2px;
+}
 
-        </style>
+.renervate-photo {
+    background-image: url(./footer_elements/longlogo.png);
+    background-size: contain;
+}
+
+
+.icon {
+    width: 20px;
+    height: 20px;
+    filter: invert(62%) sepia(26%) saturate(710%) hue-rotate(92deg) brightness(99%) contrast(92%);
+    border-style: solid;
+    border-color: var(--dodger);
+    padding: 0.3rem;
+    border-width: 0.1rem;
+}
+
+.icon:hover {
+    /* This filter method need the conversion from here: https://codepen.io/sosuke/pen/Pjoqqp */
+    filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+    border-style: dashed;
+}
+
+.icon2:hover {
+    filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+}
+
+#ltxt:hover  #licn {
+    filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+}
+
+</style>
 
 <footer>
 <div class="footer-container">
-        <div class="footer">
-            <div class="footer-heading navlinks">   
-                    <p>Pages</p>
-                <ul>
-                <li>    <a href="#" target="_blank">Home                        </a>        </li>
-                <li>    <a href="#" target="_blank">Team Members                </a>        </li>
-                <li>    <a href="#" target="_blank">Project Description         </a>        </li>
-                <li>    <a href="#" target="_blank">Project Design              </a>        </li>
-                <li>    <a href="#" target="_blank">Model                       </a>        </li>
-                <li>    <a href="#" target="_blank">Parts                       </a>        </li>
-                <li>    <a href="#" target="_blank">Proposed Implementation     </a>        </li>
-                <li>    <a href="#" target="_blank">Human Practices             </a>        </li>
-                <li>    <a href="#" target="_blank">Inclusivity Research        </a>        </li>
-                <li>    <a href="#" target="_blank">Science Communication       </a>        </li>
-                <li>    <a href="#" target="_blank">Biologix Competition        </a>        </li>
-                <li>    <a href="#" target="_blank">Judging Form                </a>        </li>
-                </ul>
-            </div>
-            <div class="footer-heading sponsors">
-                    <p>Sponsors</p>
-                <ul>
-                <li>    <a href="#" target="_blank">King's College London       </a>        </li>
-                <li>    <a href="#" target="_blank">Wellcome                    </a>        </li>
-                <li>    <a href="#" target="_blank">Promega                     </a>        </li>
-                <li>    <a href="#" target="_blank">KCLSU                       </a>        </li>
-                <li>    <a href="#" target="_blank">BBSRC                       </a>        </li>
-                <li>    <a href="#" target="_blank">SnapGene                    </a>        </li>
-                <li>    <a href="#" target="_blank">Benchling                   </a>        </li>
-                <li>    <a href="#" target="_blank">BGM Lab Tech                </a>        </li>
-                <li>    <a href="#" target="_blank">IDT                         </a>        </li>
-                </ul>
-            </div>
-            <div class="social">
-                <a class="renervate-logo" href="#" target="_top"></a>
-                
-                <div class="social-box">
-                    <p>Contact Us</p>
-                    <ul>
-                        <a href="#" target="_blank"><img class="icon" src="./footer_elements/instagram.svg"></a>                           
-                        <a href="#" target="_blank"><img class="icon" src="./footer_elements/twitter.svg"></a>                             
-                        <a href="#" target="_blank"><img class="icon" src="./footer_elements/facebook.svg"></a>                            
-                        <a href="#" target="_blank"><img class="icon" src="./footer_elements/envelope.svg"></a>
-                    </ul>                               
-                </div>
-            </div>
+<div class="footer">
+    <div class="footer-heading navlinks">   
+            <p>Pages</p>
+        <ul>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Home                                <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Team Members                        <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Project Description                 <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Project Design                      <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Model                               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Parts                               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Proposed Implementation             <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Human Practices                     <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Inclusivity Research                <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Science Communication               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Biologix Competition                <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Judging Form                        <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        </ul>
+    </div>
+    <div class="footer-heading sponsors">
+            <p>Sponsors</p>
+        <ul>
+        <li id = 'ltxt'>    <a href="#" target="_blank">King's College London               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Wellcome                            <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Promega                             <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">KCLSU                               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">BBSRC                               <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">SnapGene                            <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">Benchling                           <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">BGM Lab Tech                        <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        <li id = 'ltxt'>    <a href="#" target="_blank">IDT                                 <img id = 'licn' src="./footer_elements/arrow-up-right-circle.svg" class = "icon2"/>    </a>        </li>
+        </ul>
+    </div>
+    <div class="social">
+        <a class="social-photo renervate-photo" href="#" target="_top"></a>
+        <div class="social-box">
+            <p>Contact Us</p>
+            <ul>
+        <li>        <a href="#" target="_blank"><img class="icon" src="./footer_elements/instagram.svg">    </a> </li>                           
+        <li>        <a href="#" target="_blank"><img class="icon" src="./footer_elements/twitter.svg">      </a> </li>                             
+        <li>        <a href="#" target="_blank"><img class="icon" src="./footer_elements/facebook.svg">     </a> </li>                            
+        <li>        <a href="#" target="_blank"><img class="icon" src="./footer_elements/envelope.svg">     </a> </li>
+            </ul>                               
         </div>
-        <img src = "./footer_elements/longlogo.png" width = "100%" height = "auto" />
+        <a class="social-photo team-photo" href="#" target="_top"></a>
         
+    </div>
+</div>
+
 
 </div>
 </footer>
