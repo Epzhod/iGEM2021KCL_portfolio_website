455672ba4cd504c0f5a8d4e38fe62ab28e64fb2b
Epzhod <79798931+Epzhod@users.noreply.github.com>
2021-08-27 14:18:47 +0100

    redefined values as a separate .js document

---
+++ DICT.js
@@ -0,0 +1,109 @@
+const ECOLI = {
+    "comment" : " Most used codon for each amino acid for E.coli., Referenced on 29/06/2021 from : https://www.genscript.com/tools/codon-frequency-table",
+
+     "L" : "CTG", 
+     "F" : "TTT", 
+     "Y" : "TAT", 
+     "*" : "TAA", 
+     "H" : "CAT", 
+     "Q" : "CAG", 
+     "I" : "ATT",
+     "M" : "ATG",
+     "N" : "AAC",
+     "K" : "AAA",
+     "V" : "GTG",
+     "D" : "GAT",
+     "E" : "GAA",
+     "S" : "AGC",
+     "C" : "TGC",
+     "W" : "TGG",
+     "P" : "CCG",
+     "R" : "CGT",
+     "T" : "ACC",
+     "A" : "GCC",
+     "G" : "GGC"
+};
+
+const DROSOPHILA = { 
+
+    "comment" : " Dictionary of the most used codon for each amino acid for D. melanogaster using data from NCBI Taxonomy 26/08/2021",
+        
+                        "L" : "CTG", 
+                        "F" : "TTC", 
+                        "Y" : "TAC", 
+                        "*" : "TAA", 
+                        "H" : "CAC", 
+                        "Q" : "CAG", 
+                        "I" : "ATC",
+                        "M" : "ATG",
+                        "N" : "AAC",
+                        "K" : "AAG",
+                        "V" : "GTG",
+                        "D" : "GAC",
+                        "E" : "GAG",
+                        "S" : "TCC",
+                        "C" : "TGC",
+                        "W" : "TGG",
+                        "P" : "CCC",
+                        "R" : "CGC",
+                        "T" : "ACC",
+                        "A" : "GCC",
+                        "G" : "GGC"
+    };
+
+    
+const MUSCULUS = { 
+
+    "comment" : " Dictionary of the most used codon for each amino acid for Mus musculus using data from NCBI Taxonomy 26/08/2021",
+        
+                        "L" : "CTG", 
+                        "F" : "TTC", 
+                        "Y" : "TAC", 
+                        "*" : "TGA", 
+                        "H" : "CAC", 
+                        "Q" : "CAG", 
+                        "I" : "ATC",
+                        "M" : "ATG",
+                        "N" : "AAC",
+                        "K" : "AAG",
+                        "V" : "GTG",
+                        "D" : "GAC",
+                        "E" : "GAG",
+                        "S" : "AGC",
+                        "C" : "TGC",
+                        "W" : "TGG",
+                        "P" : "CCC",
+                        "R" : "AGG",
+                        "T" : "ACC",
+                        "A" : "GCC",
+                        "G" : "GGC"
+    };
+
+const SAPIENS = { 
+
+    "comment" : " Dictionary of the most used codon for each amino acid for Homo Sapiens using data from NCBI Taxonomy 26/08/2021",
+        
+                        "L" : "CTG", 
+                        "F" : "TTC", 
+                        "Y" : "TAC", 
+                        "*" : "TGA", 
+                        "H" : "CAC", 
+                        "Q" : "CAG", 
+                        "I" : "ATC",
+                        "M" : "ATG",
+                        "N" : "AAC",
+                        "K" : "AAG",
+                        "V" : "GTG",
+                        "D" : "GAC",
+                        "E" : "GAG",
+                        "S" : "AGC",
+                        "C" : "TGC",
+                        "W" : "TGG",
+                        "P" : "CCC",
+                        "R" : "AGG",
+                        "T" : "ACC",
+                        "A" : "GCC",
+                        "G" : "GGC"
+    };    
+
+    
